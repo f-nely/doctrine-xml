06d4e5fba269c36ce5439b041b8d5e508a4f7c78
nely de faria <nelly.nf54@gmail.com>
2021-02-16 18:06:22 -0300

    Adiciona classe Filme

---
+++ alura-filmes/src/Entity/Filme.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Alura\Doctrine\Entity;
+
+
+class Filme
+{
+    private $id;
+    private $titulo;
+    private $sinopse;
+    private $anoLancamento;
+    private $ultimaAtualizacao;
+}
