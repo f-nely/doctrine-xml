5fd0a93501e9798345afd0d831c7fa7d0a90ef6f
nely de faria <nelly.nf54@gmail.com>
2021-02-21 17:34:22 -0300

    Instancia classe Idioma

--- alura-filmes/teste.php
+++ alura-filmes/teste.php
@@ -2,14 +2,20 @@
 
 use Alura\Doctrine\Entity\Ator;
 use Alura\Doctrine\Entity\Filme;
+use Alura\Doctrine\Entity\Idioma;
 
 require_once 'vendor/autoload.php';
 
 $em = (new \Alura\Doctrine\Helper\EntityManagerCreator())->criaEntityManager();
 
 $ator = new Ator(null, 'Vinicius', 'Dias');
-$filme = new Filme(null, 'As longas tranças do careca', '2019');
-$filme2 = new Filme(null, 'A volta dos que não foram', '2019');
+
+$portugues = new Idioma(null, 'Português');
+$alemao = new Idioma(null, 'Alemão');
+$ingles = new Idioma(null, 'Inglês');
+
+$filme = new Filme(null, 'As longas tranças do careca', '2019', $portugues, $alemao);
+$filme2 = new Filme(null, 'A volta dos que não foram', '2019', $portugues, $ingles);
 
 $ator->addFilme($filme);
 $ator->addFilme($filme2);
